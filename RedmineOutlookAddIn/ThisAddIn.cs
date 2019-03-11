027e0af0ee677ab5990ebcff80bbd45b8eabbe96
Varvara Dvortsova <dvortsova.varvara@yandex.ru>
2019-03-11 13:02:34 +0300

    addTask addMessage

--- RedmineOutlookAddIn/ThisAddIn.cs
+++ RedmineOutlookAddIn/ThisAddIn.cs
@@ -5,34 +5,203 @@ using System.Text;
 using System.Xml.Linq;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
+using Redmine.Net.Api;
+using Redmine.Net.Api.Types;
 
 namespace RedmineOutlookAddIn
 {
-    public partial class ThisAddIn
-    {
-        private void ThisAddIn_Startup(object sender, System.EventArgs e)
-        {
+   
+	public partial class ThisAddIn
+	{
 
-        }
+		private Office.CommandBar _objMenuBar;
+		private Office.CommandBarPopup _objNewMenuBar;
+		private Outlook.Inspectors Inspectors;
+		private Office.CommandBarButton _objButton;//Кнопка в надстройке
+		RedmineManager manager;
 
-        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
-        {
-            // Note: Outlook no longer raises this event. If you have code that 
-            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
-        }
+		private string menuTag = "MyfirstPlugin";
+		private void ThisAddIn_Startup(object sender, System.EventArgs e)
+		{
+			this.MyMenuBar();
+			string host = "http://79.137.216.214/redmine/";
+			string apiKey = "4444025d7e83c49e92466b5399ba7ee06c464637";
 
-        #region VSTO generated code
+			manager = new RedmineManager(host, apiKey);
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InternalStartup()
-        {
-            this.Startup += new System.EventHandler(ThisAddIn_Startup);
-            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
-        }
-        
-        #endregion
-    }
+			Inspectors = this.Application.Inspectors;
+		}
+
+		private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+		{
+			// Note: Outlook no longer raises this event. If you have code that 
+			//    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+		}
+
+
+		#region "Outlook07 Menu"
+		private void MyMenuBar()
+		{
+			this.ErsMyMenuBar();
+
+			try
+			{
+
+
+				_objMenuBar = this.Application.ActiveExplorer().CommandBars.ActiveMenuBar;
+				_objNewMenuBar = (Office.CommandBarPopup)
+								 _objMenuBar.Controls.Add(Office.MsoControlType.msoControlPopup
+														, missing
+														, missing
+														, missing
+														, false);
+
+				if (_objNewMenuBar != null)
+				{
+
+
+					_objNewMenuBar.Caption = "My Plugin";
+					_objNewMenuBar.Tag = menuTag;
+
+
+					_objButton = (Office.CommandBarButton)_objNewMenuBar.Controls.
+					Add(Office.MsoControlType.msoControlButton, missing,
+						missing, 1, true);
+					//_objButton.Picture = "redmine.ico";
+
+
+					_objButton.Caption = "Hello World";
+					//Icon 
+					_objButton.FaceId = 5000;
+					_objButton.Tag = "ItemTag";
+					//EventHandler
+					_objButton.Click += new Office._CommandBarButtonEvents_ClickEventHandler(_objButton_Click);
+					_objNewMenuBar.Visible = true;
+				}
+			}
+			catch (System.Exception ex)
+			{
+				System.Windows.Forms.MessageBox.Show("Error: " + ex.Message.ToString()
+												   , "Error Message");
+			}
+
+		}
+		#endregion
+		#region VSTO generated code
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(ThisAddIn_Startup);
+			this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+		}
+		#endregion
+
+		#region "Event Handler"
+		#region "Menu Button"
+
+		private void _objButton_Click(Office.CommandBarButton ctrl, ref bool cancel)
+		{
+			try
+			{
+				AddTask();
+
+			}
+			catch (System.Exception ex)
+			{
+				System.Windows.Forms.MessageBox.Show("Error " + ex.Message.ToString() + $"/n{ex.StackTrace}");
+			}
+		}
+
+
+		#endregion
+
+		#region "Remove Existing"
+
+		private void ErsMyMenuBar()
+		{
+			// If the menu already exists, remove it.
+			try
+			{
+				Office.CommandBarPopup _objIsMenueExist = (Office.CommandBarPopup)
+					this.Application.ActiveExplorer().CommandBars.ActiveMenuBar.
+					FindControl(Office.MsoControlType.msoControlPopup
+							  , missing
+							  , menuTag
+							  , true
+							  , true);
+
+				if (_objIsMenueExist != null)
+				{
+					_objIsMenueExist.Delete(true);
+				}
+			}
+			catch (System.Exception ex)
+			{
+				System.Windows.Forms.MessageBox.Show("Error: " + ex.Message.ToString()
+												   , "Error Message");
+			}
+		}
+		private void AddTask()
+		{
+			try
+			{
+
+				Outlook.TaskItem newTaskItem =
+					(Outlook.TaskItem)this.Application.CreateItem(Outlook.OlItemType.olTaskItem);
+				newTaskItem.StartDate = DateTime.Now.AddHours(2);
+
+
+				newTaskItem.Body = "Try to create a task";
+				//Create a issue.
+
+				newTaskItem.Save();
+				newTaskItem.Display("True");
+				var newIssue = new Issue { Subject = newTaskItem.Subject, Project = new IdentifiableName { Id = 1 } };
+				manager.CreateObject(newIssue);
+
+			}
+			catch (Exception ex)
+			{
+
+				MessageBox.Show("The following error occurred: " + ex.Message); throw;
+			}
+		}
+		private void AddAppointment()
+		{
+			try
+			{
+				Outlook.AppointmentItem newAppointment =
+					(Outlook.AppointmentItem)
+				this.Application.CreateItem(Outlook.OlItemType.olAppointmentItem);
+				newAppointment.Start = DateTime.Now.AddHours(2);
+				newAppointment.End = DateTime.Now.AddHours(3);
+				newAppointment.Location = "ConferenceRoom #2345";
+				newAppointment.Body =
+					"We will discuss progress on the group project.";
+				newAppointment.AllDayEvent = false;
+				newAppointment.Subject = "Group Project";
+				newAppointment.Recipients.Add("Roger Harui");
+				Outlook.Recipients sentTo = newAppointment.Recipients;
+				Outlook.Recipient sentInvite = null;
+				sentInvite = sentTo.Add("Holly Holt");
+				sentInvite.Type = (int)Outlook.OlMeetingRecipientType
+					.olRequired;
+				sentInvite = sentTo.Add("David Junca ");
+				sentInvite.Type = (int)Outlook.OlMeetingRecipientType
+					.olOptional;
+				sentTo.ResolveAll();
+				newAppointment.Save();
+				newAppointment.Display(true);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("The following error occurred: " + ex.Message);
+			}
+		}
+
+		#endregion
+
+		#endregion
+	}
 }
+
